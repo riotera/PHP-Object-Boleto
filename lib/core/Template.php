af37cd5fbdc72b35c4afe3aa73c7ef86e56caddf
Marco Pergola <marco.pergola@riotera.com>
2011-09-01 02:22:15 -0300

    Utilizo da nova definição de percurso

--- lib/core/Template.php
+++ lib/core/Template.php
@@ -18,31 +18,31 @@ class Template{
        Template utilizado na página
     */
     public $Template = 'default';
-    
+
     /*
        @var array $Styles
        Sequência de estilos adicionados à página
     */
     private $Styles = array();
-    
+
     /*
        @var array $Data
        Guarda o objeto pai enviado
     */
     protected $Data = array();
-    
+
     /**
       * Construtor da classe
-      * 
+      *
       * @version 0.1 19/05/2011 Initial
       */
     public function __construct(&$obj){
         $this->parent = $obj;
     }
-    
+
     /**
       * Adiciona estilos css ao template
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       *          0.2 19/05/2011 - Só um estilo igual é adicionado
       *                         - Styles podem ser adicionados a partir dos
@@ -66,12 +66,12 @@ class Template{
         }
         #Guardando o estilo
         $this->Styles[$filename] = $media;
-        
+
         return $this;
     }
     /**
       * Retorna os styles css formatados como html
-      * 
+      *
       * @version 0.1 19/05/2011 Initial
       */
     public function getStyles(){
@@ -79,34 +79,34 @@ class Template{
             $html = '';
             foreach($this->Styles as $file => $media){
                 $html .= '<link rel="stylesheet" type="text/css" media="'
-                      . $media . '" href="' .  OB::url('/public/css/' . $file)
+                      . $media . '" href="' . ASSETS_ROOT . '/css/' . $file
                       . '" />' . PHP_EOL;
             }
         }
         return $html;
     }
-    
+
     /**
       * Pega o html do bloco pré-carregado
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public function getBlock($blockName, $data = array()){
         //$data = array_merge($data, array('OB' => (object)$this->Data));
-        
+
         return $this->render('/blocks/'.$blockName, $data);
     }
-    
+
     /**
       * Renderiza templates
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       *          1.0 22/05/2011 Consertado o bug dos Output bufferin
       */
     public function render($template, $data = array()){
         $data = array_merge($data, array('OB' => $this->parent));
         $template = OB_DIR . '/templates/' . $template . '.htm.php';
-        
+
         if(file_exists($template)){
             extract($data);
             ob_start();
@@ -116,38 +116,38 @@ class Template{
             throw new Exception('Template "' . $template . '" não existe.');
         }
     }
-    
+
     /**
       * Manda variáveis para os templates/blocks
-      * 
+      *
       * @version 0.1 20/05/2011 Initial
       */
     public function set($var, $value){
         $this->$var = $value;
         return $this;
     }
-    
-    
+
+
     # # # # # # # # # # # # # # # # # # # # # #
-    # # 
+    # #
     # #     SETTERS E TRATAMENTO DOS VALORES DE ENTRADA
     # #
     # # # # # # # # # # # # # # # # # # # # # #
 
-    
+
     /**
       * Configura o título da página do template
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public function setTitle($title){
         $this->Title = $title;
         return $this;
     }
-    
+
     /**
       * Define qual o template que será usado
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public function setTemplate($template){
