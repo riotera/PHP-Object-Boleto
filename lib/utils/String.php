cdf88294549bf5e117e098daaf60c04940a953d8
Marco Pergola <marco.pergola@riotera.com>
2011-09-01 07:53:52 -0300

    A solução para putAt() pode ser melhor?

--- lib/utils/String.php
+++ lib/utils/String.php
@@ -1,10 +1,10 @@
 <?php
 
 class String{
-    
-    
+
+
     protected static $self;
-    
+
     /**
       * Construtor
       *
@@ -12,13 +12,13 @@ class String{
     public function __construct(){
         mb_internal_encoding('utf-8');
     }
-    
+
     /**
       * Instância singleton
-      * 
+      *
       * @version
       *     0.1 18/05/2011 Initial
-      * 
+      *
       * @param string $text Texto a ser verificado
       * @param int $length Tamanho máximo
       */
@@ -26,16 +26,16 @@ class String{
         if(empty(self::$self)){
             self::$self = new String;
         }
-        
+
         return self::$self;
     }
-    
+
     /**
       * Corta um texto, exibindo somente os $num caracteres ao final
-      * 
+      *
       * @version
       *     0.1 18/05/2011 Initial
-      * 
+      *
       * @param string $text Texto a ser verificado
       * @param int $length Tamanho máximo
       */
@@ -43,13 +43,13 @@ class String{
         self::instance();
         return mb_substr($text, 0, $length);
     }
-    
+
     /**
       * Corta um texto, exibindo somente os $num caracteres ao final
-      * 
+      *
       * @version
       *     0.1 18/05/2011 Initial
-      * 
+      *
       * @param string $text Texto a ser verificado
       * @param int $length Tamanho máximo
       */
@@ -57,34 +57,34 @@ class String{
         self::instance();
         return mb_substr($text, -1 * $length);
     }
-    
-    
+
+
     /**
       * Corta um texto em um tamanho definido, adicionando reticências ou outro
       * indicador de continuação ao final.
-      * 
+      *
       * @version
       *     0.1 Initial
       *     0.2 09/10/2010 Adicionado suporte às funções multi-byte.
-      * 
+      *
       * @param string $text Texto a ser verificado
       * @param int $length Tamanho máximo
       * @param string $complement Complemento para o texto cortado. O padrão
       *     é reticências: "..."
       * @param bool $cut Define se as palavras podem ser cortadas, caso o
       *     tamanho máximo seja atingido no meio de uma delas.
-      *     
+      *
       */
     public function slice($text, $length, $complement = '...', $cut = false){
         $newText = mb_substr($text, 0, $length);
-        
+
         //Se na string original, o tamanho definido já representar um espaço,
         //ou seja, o tamanho requerido não decepará a palavra, ou então, se
         //é permitido cortar a palavra na metade com $cut = true
         if(mb_substr($text, $length, 1) == ' ' || $cut === true):
             return $newText . $complement;
         endif;
-        
+
         if(mb_strlen($text) > $length):
             //O último espaço encontrado na string
             $lastSpacePos = mb_strrpos($newText, " ");
@@ -94,33 +94,33 @@ class String{
             if($lastSpacePos !== false):
                 $length = $lastSpacePos;
             endif;
-            
+
             return mb_substr($newText, 0, $length) . $complement;
         else:
             return $text;
-        endif;        
+        endif;
     }
-    
+
     /**
       * Checa se a string passada está em UTF-8
       *
       * @version
       *     0.1 09/10/2010 Initial
-      * 
+      *
       * @param string $text
       * @return bool TRUE caso a string esteja em utf-8
       */
     public function isUtf8($text){
         return mb_check_encoding($text, 'utf-8');
     }
-    
-    
+
+
     /**
       * Checa se a string passada está em UTF-8
       *
       * @version
       *     0.1 09/10/2010 Initial
-      *     
+      *
       * @param string $text
       * @param mixed $verifyFollowings A lista dos charsets que serão usados
       *     para verificar se a string informada está em um deles.
@@ -129,12 +129,12 @@ class String{
       */
     public function getCharset($text,
                                $verifyFollowings = array('utf-8', 'iso-8859-1')){
-        
+
         //O "x" corrige um bug da função "mb_detect_string", que falha caso
         //a última letra da string seja acentuada.
         return mb_detect_encoding($text . 'x', $verifyFollowings);
     }
-    
+
     /**
       * Converte para utf8 se ainda não estiver
       *
@@ -148,10 +148,10 @@ class String{
         if(!$this->isUtf8($text)):
             return utf8_encode($text);
         endif;
-        
+
         return $text;
     }
-    
+
     public static function insert($string, $data) {
         foreach($data as $key => $value):
             $regex = '%(:' . $key . ')%';
@@ -166,6 +166,7 @@ class String{
     //18/05/2011
     public static function putAt($text, $put, $at){
         return self::left($text, $at) . $put . mb_substr($text, $at);
+		//return substr_replace($text, $put, $at, 0);
     }
     //18/05/2011
     public static function applyMask($text, $mask){
@@ -176,15 +177,15 @@ class String{
 
         for($i = 0, $j = 0; $i < $length; $i++, $j++){
             if(!isset($text[$i]) || !isset($mask[$j])) break;
-            
+
             if(in_array($mask[$j], $special)){
                 $buff .= $mask[$j];
                 $j++;
             }
                 $buff .= $text[$i];
         }
-        
+
         return $buff;
     }
-    
+
 }
