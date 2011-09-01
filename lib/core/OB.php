9591bcd88b2bc72805604033dc98ce68c9393a89
Marco Pergola <marco.pergola@riotera.com>
2011-09-01 07:52:34 -0300

    Adicionada chamada a getTamanhos() para fazer o merge com eventuais valores de $carteiras (precisa rever essa solução)

--- lib/core/OB.php
+++ lib/core/OB.php
@@ -1,61 +1,61 @@
 <?php
 
 class OB{
-    
+
     /*
         @var object $Vendedor
         Dados do cedente (banco ou quem vai receber)
      */
-    public $Vendedor;    
-    
+    public $Vendedor;
+
     /*
         @var object $Cliente
         Dados do Sacado (cliente. quem pagará a conta)
      */
     public $Cliente;
-    
+
     /*
         @var object $Boleto
         Dados de configurações do banco, layout, etc.
      */
     public $Boleto;
-    
+
     /*
         @var object $Template
         Dados de configurações dos templates
      */
     public $Template;
-    
+
     /*
         @var object $Banco
         Dados de configuração do Layout para o banco escolhido
      */
     public $Banco;
-    
+
     /*
         @var array $Data
         Dados utilizados na geração do código de barras e da linha digitável
      */
     public $Data = array();
-    
-    
+
+
     /**
       * Construtor
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public function __construct($codigoBanco){
         $this->loadBanco($codigoBanco);
-        
+
         $classes = array('Vendedor', 'Cliente', 'Boleto', 'Configuracao', 'Template');
         foreach($classes as $class){
             $this->$class = new $class($this);
         }
     }
-    
+
     /**
       * Carrega o plugin informado
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public function plugin($pluginName){
@@ -65,10 +65,10 @@ class OB{
         return Plugin::load($pluginName);
 
     }
-    
+
     /**
       * Carrego os dados do banco desejado
-      * 
+      *
       * @version 0.1 20/05/2011 Initial
       *          0.2 20/05/2011 Verificando se algo já foi carregado
       *          0.3 27/05/2011 Parâmetro $codigoBanco adicionado.
@@ -79,30 +79,30 @@ class OB{
         if(!empty($this->Banco)){
             return $this->Banco;
         }
-        
+
         #Instância do Layouts pai
         $banco = new Banco;
-        
+
         #Todos os layouts dos bancos estendem o layout pai. Carrego o layout
         #específico para o banco em questão
         $this->Banco = $banco->load($codigoBanco, $this);
-        
+
         return $this->Banco;
     }
-    
+
     /**
       * Calcula o fator de vencimento, que é a quantidade de dias desde
       * a data-base 07/10/1997
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public static function fatorVencimento($dia, $mes, $ano){
         $timestampVencimento = (int) (mktime(0, 0, 0, $mes, $dia, $ano) / (24 * 60 * 60));
         $timestampDatabase = (int) (mktime(0, 0, 0 , 10, 7, 1997) / (24 * 60 * 60));
-        
+
         return abs($timestampVencimento - $timestampDatabase);
     }
-    
+
     /**
       * Transforma um fator de vencimento em uma data
       *
@@ -111,10 +111,10 @@ class OB{
     public static function fatorVencimentoParaData($fator, $formato = 'd/m/Y'){
         return date($formato, ((strtotime('1997-10-7 00:00:00')/86400) + $fator) * 86400);
     }
-    
+
     /**
       * Gera o código numérico que será a base para o código de barras
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       *          0.2 20/05/2011 - Modificações gerais;
       *                         - Uso das variáveis de layout externas;
@@ -131,13 +131,13 @@ class OB{
         if(empty($this->Boleto->CodigoBarras)){
             #Padroniza os dados necessários de acordo com Layout do banco
             $this->normalizeData();
-            
+
             #Verifica alguma particularidade do banco na geração do código
             $this->Banco->particularidade($this);
-            
+
             #Verifica os campos obrigatórios
             $this->Banco->verificaObrigatorios($this->Data);
-
+//print_r($this->Banco->layoutCodigoBarras);
             #Insere os valores de $this->data no layout do codigo de barras
             $cod = String::insert($this->Banco->layoutCodigoBarras, $this->Data);
 
@@ -146,14 +146,15 @@ class OB{
 
             #Inserindo o dígito verificador exatamente na posição 4, iniciando em 0.
             $this->Boleto->CodigoBarras = String::putAt($cod, $dv, 4);
+//echo $this->Boleto->CodigoBarras;echo '<br>';exit();
         }
-        
+
         return $this->Boleto->CodigoBarras;
     }
-    
+
     /**
       * Normaliza os dados da propriedade, unificando a esse trabalho
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       *          0.2 20/05/2011 Agora verifica se a propriedade já está
       *                 preenchida, salva os dados em uma propriedade de
@@ -178,12 +179,13 @@ class OB{
                 'CodigoCedente' => $this->Vendedor->CodigoCedente,
                );
 
+			$this->Banco->getTamanhos();
             foreach($this->Data as $var => $value){
                 if(array_key_exists($var, $this->Banco->tamanhos)){
                     $this->Data[$var] = self::normalize($this->Data[$var], $this->Banco->tamanhos[$var]);
                 }
             }
-            
+
             $this->Data['Vencimento'] = $this->Boleto->Vencimento;
             $this->Data['DigitoAgencia'] = Math::Mod11($this->Data['Agencia']);
             $this->Data['DigitoConta'] = Math::Mod11($this->Data['Conta']);
@@ -195,7 +197,7 @@ class OB{
             return $this->Data;
         }
     }
-    
+
     /**
       * Gera a linha digitável já formatada. O layout só é necessário
       * para gerar o código de barras. Para gerar a linha digitável,
@@ -204,7 +206,7 @@ class OB{
       * Campo1 - Posições de 1-4 e 20-24, Campo2 - Posições 25-34,
       * Campo3 - Posições 35-44, Campo4 - Posição 5 e
       * Campo5 - Posições 6-19.
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       *          1.0 27/05/2011 Mudança total em linha digitavel.
       */
@@ -218,7 +220,7 @@ class OB{
         //  10491.87006 00900.000001 00012.345674 7 49860000100000
         //  10491.87006 00900.000001 00012.345674 7 49860000100000
         // 104918700    0090000000   00012345677
-        
+
         //Campo1 - Posições de 1-4 e 20-24
         $linhaDigitavel = substr($codigo, 0, 4) . substr($codigo, 19, 5)
         //Campo2 - Posições 25-34
@@ -229,7 +231,7 @@ class OB{
                          .substr($codigo, 4, 1)
         //Campo5 - Posições 6-19
                          .substr($codigo, 5, 14);
-        
+
         $dv1 = Math::Mod10(substr($linhaDigitavel, 0, 9));
         $dv2 = Math::Mod10(substr($linhaDigitavel, 9, 10));
         $dv3 = Math::Mod10(substr($linhaDigitavel, 19, 10));
@@ -238,52 +240,52 @@ class OB{
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv3, 29);
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv2, 19);
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv1, 9);
-       
+
         #Aplicando A linha digitável gerada à sua máscara
         $linhaDigitavel = String::applyMask($linhaDigitavel, $this->Banco->mascaraLinhaDigitavel);
 
         return $linhaDigitavel;
     }
-    
+
     /*public function linhaDigitave(){
         $codigo = $this->geraCodigo();
-        
+
         #Inicio $data vazia
         $data = array();
-        
+
         #A partir das posições indicadas pelo layout, separo os dados dentro
         #do código em variáveis normais
         foreach($this->Banco->posicoes as $var => $substr){
             $data[$var] = substr($codigo, $substr[0], $substr[1]);
         }
-        
+
         #Aplico no layout da linha digitável os dados da variável $data
         $linhaDigitavel = String::insert($this->Banco->layoutLinhaDigitavel, $data);
 
-        #Calculando o dv vindo do código de barras, que 
+        #Calculando o dv vindo do código de barras, que
         #fica exatamente na posição 4, iniciando em 0
         $dv = $codigo[4];
-        
+
         #Calculando os dígitos verificadores dos subgrupos
         $dv1 = Math::Mod10(substr($linhaDigitavel, 0, 9));
         $dv2 = Math::Mod10(substr($linhaDigitavel, 9, 10));
         $dv3 = Math::Mod10(substr($linhaDigitavel, 20, 10));
-        
+
         #Inserindo os DVs em seus lugares
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv1, 9);
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv2, 20);
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv3, 31);
         $linhaDigitavel = String::putAt($linhaDigitavel, $dv, 32);
-       
+
         #Aplicando A linha digitável gerada à sua máscara
         $linhaDigitavel = String::applyMask($linhaDigitavel, $this->Banco->mascaraLinhaDigitavel);
-        
+
         return $linhaDigitavel;
     }/**/
-    
+
     /**
       * Renderiza o template
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       *          0.2 20/05/2011 Retirado o echo
       *          1.0 20/05/2011 Movido da classe Boleto para a classe OB
@@ -292,37 +294,37 @@ class OB{
         $this->Template->render($this->Template->Template);
         flush();
     }
-    
+
     # # # # # # # # # # # # # # # # # # # # # #
-    # # 
+    # #
     # #     FUNÇÕES AUXILIARES
     # #
     # # # # # # # # # # # # # # # # # # # # # #
 
-    
+
     /**
       * Pega uma url relativa
-      * 
+      *
       * @version 0.1 19/05/2011 Initial
       */
     public static function url($url = null){
         return preg_replace('([\\\/]+)','/',dirname(dirname(dirname(substr(__FILE__, strlen($_SERVER['DOCUMENT_ROOT']))))) . '//' . $url);
     }
-    
+
     /**
       * Completa com zeros adicionais à esquerda até o valor informado
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
    public static function zeros($text, $length, $cut = false){
         return str_pad($text, $length, '0', STR_PAD_LEFT);
     }
-    
+
     /**
       * Completa com zeros adicionais à esquerda até o valor informado,
       * alterando a variável original, e cortando caso o valor tenha
       * mais caracteres que o permitido
-      * 
+      *
       * @version 0.1 18/05/2011 Initial
       */
     public static function normalize(&$var, $length){
